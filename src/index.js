2ff3ee91fbe44312aef49b4554d492c28d3919d7
sayantan01 <cavi4762@gmail.com>
2020-03-29 18:31:47 +0530

    ready to deploy on Heroku

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const express=require('express');
 const app=express();
 const path=require('path');
 const bodyParser=require('body-parser');
-
 const mongoose=require('mongoose'); //ODM(Object document modelling)library for MongoDB
+
+let PORT=process.env.PORT||8080;
+
 const uri="mongodb+srv://sayantan:mydatabase0@cluster0-ve4hp.gcp.mongodb.net/test?retryWrites=true&w=majority";   //uniform resource identifier=URL+URN
-mongoose.connect(uri,{useNewUrlParser:true, useCreateIndex:true, useUnifiedTopology:true, useFindAndModify:false})
+mongoose.connect(process.env.MONGODB_URI||uri,{useNewUrlParser:true, useCreateIndex:true, useUnifiedTopology:true, useFindAndModify:false})
     .catch((err)=>console.log(err));
 mongoose.connection.once('open',()=>console.log('successfully connected to db'));
 
@@ -20,4 +22,4 @@ app.all('*',(req,res)=>{
     res.status(404).send("<h1>404 Not Found</h1>");
 });
 
-app.listen(8080,()=>console.log('server up on port 8080'));
+app.listen(PORT,()=>console.log(`server up on port ${PORT}`));
